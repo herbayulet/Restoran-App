502b92eda2ab9f2c192f4d4722ac4a9009551c87
herbayulet <sayaherbayu@gmail.com>
2023-09-27 10:37:33 +0700

    fixing change style from tailwind to

--- components/Layout.tsx.tsx
+++ components/Layout.tsx.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React, { Children } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { TouchableOpacity } from 'react-native-gesture-handler'
@@ -13,20 +13,20 @@ interface LayoutProps {
 const Layout:React.FC<LayoutProps> = ({children, hapus}) => {
   const {deleteAllCollection} = useRestoranScreens()
   return (
-    <View className="flex-1 bg-white min-h-full w-full">
+    <View style={styles.main}>
       <StatusBar style="dark" />
-      <View className="mx-3 pt-5 flex-row justify-between items-center">
+      <View style={styles.container}>
         <View>
-          <Text className="text-slate-900 text-xl font-semibold">
+          <Text style={styles.text1}>
             [Contoh] Sistem Restoran
           </Text>
-          <Text className="text-slate-300 text-base font-semibold">
+          <Text style={styles.text2}>
             Ambisius Coding
           </Text>
         </View>
-        <TouchableOpacity className='border border-black py-3 px-2 rounded-md gap-x-2 flex-row items-center' onPress={ deleteAllCollection}>
+        <TouchableOpacity style={styles.button} onPress={ deleteAllCollection}>
           <FontAwesome name="recycle" size={16} color="black" />
-            <Text>Reset</Text>
+            <Text style={{marginLeft: 20}}>Reset</Text>
         </TouchableOpacity>
       </View>
       {children}
@@ -35,3 +35,45 @@ const Layout:React.FC<LayoutProps> = ({children, hapus}) => {
 }
 
 export default Layout
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: 'white',
+    minHeight: '100%',
+    width: '100%',
+    marginTop: 30,
+    marginVertical: 10,
+    paddingVertical: 30
+  },
+  container: {
+    marginHorizontal: 10,
+    marginVertical: 10,
+    paddingTop: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  text1: {
+    color: 'slategray',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  text2: {
+    color: 'lightslategray',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+
+  button:{
+    borderWidth: 1,
+    borderColor: 'black',
+    paddingVertical: 10,
+    paddingHorizontal: 8,
+    borderRadius: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    
+  }
+
+})
